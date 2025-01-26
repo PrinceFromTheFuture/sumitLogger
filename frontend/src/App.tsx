a0583282cd05d1c8c9a166109de31b1edf16dd80
Amir Waisblay <amirwais.projects@gmail.com>
2025-01-26 16:32:14 +0200

    exist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import { Drawer, DrawerContent, DrawerTrigger } from "./components/ui/drawer";
 import "./index.css";
 function App() {
+  const [meetings, setMeetings] = useState();
+  useEffect(() => {}, []);
   return (
     <div className=" p-4  'w-full  flex justify-center flex-col items-center bg-lightblue min-h-[100vh]">
       <div className=" text-2xl text-darkblue font-bold mb-2">Sumit recorder</div>
---
+++ frontend/src/MeetingCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Meeting } from "@db/db/schema";
+import { Drawer, DrawerTrigger } from "./components/ui/drawer";
+function MeetingCard({ meeting: Meeting }) {
+  return (
+    <Drawer>
+      <DrawerTrigger>fd</DrawerTrigger>
+    </Drawer>
+  );
+}
+
+export default MeetingCard;
